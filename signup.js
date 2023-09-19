24c1e1c3f6983ace47f95832b5ca24309c1ba49c
RUTH ARIMUK <117799348+rarimuk@users.noreply.github.com>
2023-09-19 11:52:28 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,2 +1,12 @@
-function createParagraph ()]
+function createParagraph (){
   const para = 
+    document.createElement("p");
+  para.textContent = "Enter your username;";
+  document.body.appendChild(para);
+}
+const buttons = 
+  document.querrySelectorAll("button");
+for (const button of buttons)
+  {
+    button.addEventListener ("Click", create Paragraph)"
+  }
